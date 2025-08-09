c3aaac3930925c72088a938ccac9de69ee079fbc
Rollie Parrish <rparrish@flightweb.com>
2025-08-09 10:12:12 -0700

    added create/modified_dttm

--- mimic-iv-demo/src/encounter_stg_update.sql
+++ mimic-iv-demo/src/encounter_stg_update.sql
@@ -1,10 +1,24 @@
 -- Databricks notebook source
-
 --CREATE SCHEMA metastore.ccdm_stg;
 
 CREATE OR REPLACE TABLE metastore.mimic_iv_demo.ccdm_stg_encounter AS 
 (
 SELECT * FROM metastore.mimic_iv_demo.ccdm_v_encounter 
 WHERE admit_min BETWEEN DATE_PART('MINUTE', current_timestamp()) - INT(:lookback_mins) AND DATE_PART('MINUTE', current_timestamp())
-)
+);
 
+ALTER TABLE metastore.mimic_iv_demo.ccdm_stg_encounter
+ADD COLUMNS 
+  created_dttm TIMESTAMP, -- DEFAULT CURRENT_TIMESTAMP(), 
+  modified_dttm TIMESTAMP; -- DEFAULT CURRENT_TIMESTAMP();
+
+UPDATE metastore.mimic_iv_demo.ccdm_stg_encounter
+SET 
+  created_dttm = CURRENT_TIMESTAMP(),
+  modified_dttm = CURRENT_TIMESTAMP();
+
+
+-- COMMAND ----------
+
+CREATE TABLE IF NOT EXISTS metastore.mimic_iv_demo.ccdm_encounter AS 
+SELECT * FROM metastore.mimic_iv_demo.ccdm_stg_encounter LIMIT 0;
